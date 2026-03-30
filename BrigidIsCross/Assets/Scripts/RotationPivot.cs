7e71cab4a109e9458ccba88b60df157a898b77c7
joe o' shea <joedoshea15@gmail.com>
2026-03-30 17:18:07 +0100

    Player Rotates Towards Cursor
    Deleted Old Scripts and Scenes for previous in-class prototype. Altered Project to support top down perspective rather than the old FPS.

---
+++ BrigidIsCross/Assets/Scripts/RotationPivot.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotationPivot : MonoBehaviour
+{
+    // Player rotates their aim based off their mouse or touch screen location
+    //As long as your mouse is over an object on the layer "Ground" the player will rotate towards it
+
+    [SerializeField] Camera mainCamera;
+    [SerializeField] LayerMask groundLayer;
+
+    void Update()
+    {
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f, groundLayer))
+            {
+                Vector3 targetPosition = hit.point;
+                Vector3 direction = targetPosition - transform.position;
+                direction.y = 0f;
+
+                if (direction != Vector3.zero)
+                {
+                    Quaternion lookRotation = Quaternion.LookRotation(direction);
+                    transform.rotation = Quaternion.Euler(0f, lookRotation.eulerAngles.y,  0f);
+
+                }
+
+            }
+        }
+    }
+}
