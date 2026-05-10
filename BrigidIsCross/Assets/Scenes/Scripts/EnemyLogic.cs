f557dbff596bc3787b722876e4f94ff6b30e770a
Joe O'Shea <joedoshea15@gmail.com>
2026-05-10 12:34:13 +0100

    Final Enemy Prefab with kick delay added

--- BrigidIsCross/Assets/Scenes/Scripts/EnemyLogic.cs
+++ BrigidIsCross/Assets/Scenes/Scripts/EnemyLogic.cs
@@ -1,11 +1,28 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyLogic : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
+    private void Awake()
+    {
+        //add delay to the idle so kick is not triggered at same time by all enemies
+        float index = Random.Range(0.1f, 1.3f);
+        StartCoroutine(Delay(index));
+    }
+
     //Call in Projectile Logic Script
     public void Hit()
     {
         //add animation and sfx here
-        Destroy(gameObject);
+        animator.SetTrigger("Hit");
+    }
+
+    private IEnumerator Delay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        animator.SetBool("Delay", true);
     }
 }
