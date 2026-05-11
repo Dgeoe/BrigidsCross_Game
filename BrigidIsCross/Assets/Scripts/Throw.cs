98e686ff40e8b885c7ad3ad6f5e1fee502a938b8
Joe O'Shea <joedoshea15@gmail.com>
2026-05-11 14:27:56 +0100

    Game is Fully Functional on Mobile

--- BrigidIsCross/Assets/Scripts/Throw.cs
+++ BrigidIsCross/Assets/Scripts/Throw.cs
@@ -1,66 +1,78 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Throw : MonoBehaviour
 {
-    
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform firePoint;
-    [SerializeField] private Camera mainCamera;
-    [SerializeField] private LayerMask groundLayer;
     [SerializeField] public int shurikens;
-    public static Throw Instance;
-
-    private BrigidInputActions inputActions;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip throwSound;
     [SerializeField] private Animator animator;
-    
+    public static Throw Instance;
+
+    private bool wasPressing;
+
     private void Awake()
     {
-        inputActions = new BrigidInputActions();
-        if (Instance == null)
+        Instance = this;
+    }
+
+    void Update()
+    {
+        bool isPressing = false;
+
+        // Mobile
+        if (Touchscreen.current != null)
         {
-            Instance = this;
+            isPressing =
+                Touchscreen.current.primaryTouch.press.isPressed;
+        }
+        // Desktop
+        else if (Mouse.current != null)
+        {
+            isPressing = Mouse.current.leftButton.isPressed;
         }
 
-        if (audioSource == null) gameObject.AddComponent<AudioSource>();
-        audioSource = GetComponent<AudioSource>();
-    }
-    private void OnEnable()
-    {
-        inputActions.PlayerBase.Touch.Enable();
-        inputActions.PlayerBase.Touch.started += OnPress;
-        inputActions.PlayerBase.Touch.canceled += OnRelease;
+        // PRESS START
+        if (isPressing && !wasPressing)
+        {
+            BeginAim();
+        }
+
+        // RELEASE
+        if (!isPressing && wasPressing)
+        {
+            ReleaseThrow();
+        }
+
+        wasPressing = isPressing;
     }
 
-    private void OnDisable()
-    {
-        inputActions.PlayerBase.Touch.started -= OnPress;
-        inputActions.PlayerBase.Touch.canceled -= OnRelease;
-        inputActions.PlayerBase.Touch.Disable();
-    }
-
-    private void OnPress(InputAction.CallbackContext ctx)
+    private void BeginAim()
     {
         if (IsTouchOverUI()) return;
 
-        //Enable Throw Point
-        if (shurikens > 0) firePoint.gameObject.SetActive(true);
+        if (shurikens > 0)
+        {
+            firePoint.gameObject.SetActive(true);
+        }
+
         animator.SetBool("Aiming", true);
 
-        if (shurikens == 0) EndGame_Manager.Instance.ThrowCheck();
+        if (shurikens == 0)
+        {
+            EndGame_Manager.Instance.ThrowCheck();
+        }
     }
 
-    private void OnRelease(InputAction.CallbackContext ctx)
+    private void ReleaseThrow()
     {
         animator.SetBool("Aiming", false);
+
         if (IsTouchOverUI()) return;
 
-        //Fire Projectile
         if (shurikens > 0)
         {
             Shoot();
@@ -70,41 +82,47 @@ public class Throw : MonoBehaviour
 
     private void Shoot()
     {
-        animator.SetBool("Aiming", true);
-        shurikens --;
+        shurikens--;
+
         Destroy(ProjectileCount.Instance.crosses[shurikens]);
+
         firePoint.gameObject.SetActive(false);
+
         RaycastHit hit;
-        Vector3 targetPoint;
 
-        if (Physics.Raycast(firePoint.position, firePoint.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        if (Physics.Raycast(
+            firePoint.position,
+            firePoint.forward,
+            out hit,
+            Mathf.Infinity))
         {
-            targetPoint = hit.point;
-        }
-        else
-        {
-            return;
+            Vector3 direction =
+                (hit.point - firePoint.position).normalized;
+
+            GameObject projectile = Instantiate(
+                projectilePrefab,
+                firePoint.position,
+                Quaternion.identity);
+
+            projectile.GetComponent<ProjectileLogic>()
+                .Initialize(direction);
         }
 
-        Vector3 direction = (targetPoint - firePoint.position).normalized;
-        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
-        projectile.GetComponent<ProjectileLogic>().Initialize(direction);
+        audioSource.volume -= 0.3f;
 
-        //PLAY THROW SOUND (protein tubes *beep* with that white sauce *beep*)
-        audioSource.volume = audioSource.volume - 0.3f;
-        if (audioSource.volume <= 0) audioSource.volume = 0.3f;
+        if (audioSource.volume <= 0)
+            audioSource.volume = 0.3f;
+
         audioSource.PlayOneShot(throwSound);
-        animator.SetTrigger("Throw");
-        animator.SetBool("Aiming", false);
-    }
 
+        animator.SetTrigger("Throw");
+    }
 
     private bool IsTouchOverUI()
     {
         if (EventSystem.current == null)
             return false;
 
-        // Mobile touch
         if (Touchscreen.current != null &&
             Touchscreen.current.primaryTouch.press.isPressed)
         {
@@ -113,7 +131,6 @@ public class Throw : MonoBehaviour
             );
         }
 
-        // Mouse/Desktop
         return EventSystem.current.IsPointerOverGameObject();
     }
 }
